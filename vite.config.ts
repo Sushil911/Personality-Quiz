6a942e267d7ba22f1aa0ddc1fbd980f11b82821e
Sushil911 <Sushilbasyal30@gmail.com>
2025-01-25 15:23:47 +0545

    Changed some file folders

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/Personality-Quiz/',
   server: {
     host: "::",
     port: 8080,
